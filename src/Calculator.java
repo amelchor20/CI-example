3f89d4184468b611b0e51981defe140104817768
amelchor20 <amelchor@syesoftware.com>
2015-07-03 19:20:32 -0500

    Update Calculator.java
    se separan los numeros por tokens

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,17 @@
+import java.util.String.*;
 
 public class Calculator {
 
 	
 	public int Add(String numbers){
 		
-		return 0;
+		int i = 0;
+		StringTokenizer nums = new StringTokenizer(numbers,",");
+		
+		while(nums.hasMoreTokens()){
+			i += Integer.parse(nums.nextToken());
+		}
+		return i;
 	}
 
 }
